681278720b7265d27cc81d5d1096fed853acf087
sillypears <sillypears@users.noreply.github.com>
2019-03-22 18:59:45 -0400

    Made some cool changes :)

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var favicon = require('serve-favicon');
 
 var indexRouter = require('./routes/index');
 //var apiRouter = require('./routes/api');
@@ -19,6 +20,8 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(favicon(__dirname + '/public/images/favicon.ico'));
+
 
 app.use('/', indexRouter);
 //api.use('/api', apiRouter);
