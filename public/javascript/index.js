b35b832b6f1224b720f8f0d8d18daa401149436e
sillypears <sillypears@users.noreply.github.com>
2019-03-22 14:57:46 -0400

    Why the fuck does this work

---
+++ public/javascript/index.js
@@ -0,0 +1,29 @@
+
+
+function selectEvent (eventData) {
+    var splitData = eventData.split(" ")
+    var event = splitData[0]
+    var year = splitData[1]
+
+    var request = new XMLHttpRequest()
+
+    request.open('GET', "api/" + event + "/" + year, true)
+    request.onload = function() {
+    // Begin accessing JSON data here
+        var data = JSON.parse(this.response)
+        
+        tO = '<table class="table">'
+        
+        data.forEach(element => {
+            tO = tO + "<tr>"
+            tO = tO + "<td>"+element.Game+"</td>"
+            tO = tO + "<td>"+element.Platform+"</td>"
+            tO = tO + "<td>"+element.StartTime+"</td>"
+            //tO = tO + "<td>"+element.Year+"</td>"
+            tO = tO + "</tr>"
+        });
+        tO = tO + "</table>"
+        $('#tableData').html(tO)
+    }
+    request.send()
+}
