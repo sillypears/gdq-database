70b8fe734de917bf2d769a06e7bbe8b627ab25b6
sillypears <sillypears@users.noreply.github.com>
2019-03-23 18:21:51 -0400

    I think I am okay with this.

--- public/javascript/index.js
+++ public/javascript/index.js
@@ -12,17 +12,26 @@ function selectEvent (eventData) {
     // Begin accessing JSON data here
         var data = JSON.parse(this.response)
         
-        tO = '<table class="table">'
-        
+        tO = '<table class="table table-striped table-hover table-sm table-responsive-sm">'
+        tO = tO + '<thead class="thead-dark">'
+        tO = tO + '<tr>'
+        tO = tO + '<th scope="col">Game</th>'
+        tO = tO + '<th scope="col">Runners</th>'
+        tO = tO + '<th scope="col">Platform</th>'
+        tO = tO + '<th scope="col">Start Time</th>'
+        tO = tO + '</tr>'
+        tO = tO + '</thead>'
+        tO = tO + '<tbody>'
         data.forEach(element => {
             tO = tO + "<tr>"
-            tO = tO + "<td>"+element.Game+"</td>"
+            tO = tO + "<td scope=\"row\">"+element.Game+"</td>"
             tO = tO + "<td>"+element.Players+"</td>"
             tO = tO + "<td>"+element.Platform+"</td>"
             tO = tO + "<td>"+element.StartTime+"</td>"
             //tO = tO + "<td>"+element.Year+"</td>"
             tO = tO + "</tr>"
         });
+        tO + tO + "</tbody>"
         tO = tO + "</table>"
         $('#tableData').html(tO)
     }
